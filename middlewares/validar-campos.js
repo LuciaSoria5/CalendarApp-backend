552ec5eca4cd28c0299f67ff56ef6223ee8384a9
Lucia Soria <luciacsoria5@gmail.com>
2024-08-09 16:24:10 -0300

    Add middleware validar-campos

---
+++ middlewares/validar-campos.js
@@ -0,0 +1,20 @@
+const { response } = require('express');
+const { validationResult } = require('express-validator');
+
+// el next se llama despues de cada middleware, hasta llegar al controlador
+const validarCampos = ( req, res = response, next ) => {
+    // Manejo de errores:
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+    next();
+}
+
+module.exports = {
+    validarCampos
+}
--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,18 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const router = Router();
+const { validarCampos } = require('../middlewares/validar-campos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
 
 router.post(
     '/new',
     [ // middlewares
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6})
+        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6}),
+        validarCampos
     ], 
     crearUsuario );
 
@@ -21,7 +24,8 @@ router.post(
     '/', 
     [
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6})
+        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6}),
+        validarCampos
     ],
     loginUsuario );
 
