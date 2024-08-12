ccd2ee113d7d928ffdd1fbeedadc34a75c149744
Lucia Soria <luciacsoria5@gmail.com>
2024-08-12 14:51:24 -0300

    Add CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'); // import express from 'express';
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
@@ -8,6 +9,9 @@ const app = express();
 
 // Base de datos
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Directorio público
 app.use( express.static('public') );
