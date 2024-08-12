5fb31fb924031f1bf84f98b1692bea589d700ec2
Lucia Soria <luciacsoria5@gmail.com>
2024-08-12 15:50:34 -0300

    Add validations on creating events

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,12 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events')
+const { validarCampos } = require('../middlewares/validar-campos');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -17,7 +20,14 @@ router.use( validarJWT );
 router.get('/', getEventos);
 
 // Crear eventos
-router.post('/', crearEvento);
+router.post('/', 
+    [
+        check( 'title', 'El titulo es obligatorio').not().isEmpty(),
+        check( 'start', 'La fecha de inicio es obligatoria').custom( isDate ),
+        check( 'end', 'La fecha de fin es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento);
 
 // Actualizar eventos
 router.put('/:id', actualizarEvento);
